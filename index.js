239166db43bf40dcc752050621b590929b9f548c
ganeshkumbhashi <ganeshkr2005@gmail.com>
2023-01-22 14:43:18 +0530

    Sending file contents to browser

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const req = require('express/lib/request');
+const path = require('path');
+
+
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World !!!</h1>');
+    res.sendFile(path.join(__dirname,'public','index.html'))
 })
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
