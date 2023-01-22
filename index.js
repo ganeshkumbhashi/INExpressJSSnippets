202fc826f83b9048946f88f260a3f0ed321dfc68
ganeshkumbhashi <ganeshkr2005@gmail.com>
2023-01-22 14:31:15 +0530

    Added basic express js template project

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const req = require('express/lib/request');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World</h1>');
+})
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
