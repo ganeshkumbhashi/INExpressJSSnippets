df31825e8594ada4bffeb63ec76565806089985e
ganeshkumbhashi <ganeshkr2005@gmail.com>
2023-01-22 14:47:54 +0530

    Used static folder functionality available in express

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'public','index.html'))
-})
+app.use(express.static(path.join(__dirname,'public')));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
